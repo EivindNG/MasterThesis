13ca558db27a9574bf985ee9d8b7344badc9cfc4
EivindNG <eivindng@stud.ntnu.no>
2019-02-06 13:25:58 +0100

    test

---
+++ src/main/java/Initiator.java
@@ -0,0 +1,5 @@
+public class Initiator {
+
+    public Initiator(){
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import crypto.PublicPrivatKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
+
+
+    }
+}
